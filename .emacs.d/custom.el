3b3f2898bf2b4885015f574139913552c38667d7
Frank Emrich <frank.emrich@ed.ac.uk>
2019-04-08 01:43:19 +0100

    configure emacs man to show in new frame

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote newframe))
  '(company-idle-delay nil)
  '(custom-safe-themes
    (quote
