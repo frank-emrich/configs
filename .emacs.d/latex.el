e696ecda4dceccfab5867a89cbc016189422bd5f
Frank Emrich <git@emrich.io>
2019-10-15 18:58:11 +0100

    more clever selection of latex viewer

--- .emacs.d/latex.el
+++ .emacs.d/latex.el
@@ -5,15 +5,73 @@
   :defer t
 )
 
-(defun viewer-from-ssh-client (client-string)
+
+
+(defun save-server-name (arg)
+  "write the current server name to a file"
+  (interactive "P")
+  (when
+      (and (boundp 'server-name) (stringp server-name))
+    (write-region server-name nil "/tmp/emacs-latex-server" nil 'quiet)))
+
+
+
+
+(defun TeX-view-choose ()
+  (interactive)
+  (let*
+      ((viewers (mapcar 'car TeX-view-program-list))
+      (builtin-viewers (mapcar 'car TeX-view-program-list-builtin))
+      (all-viewers (append viewers builtin-viewers))
+      (viewers-source (helm-build-sync-source "viewers"
+                 :candidates all-viewers))
+      (selection (helm :sources viewers-source
+	  :buffer "*helm select PDF viewer*")))
+
+    ; Update selection in all latex buffers
+    (dolist (b (buffer-list))
+      (with-current-buffer b
+	(when (string= "latex-mode" major-mode)
+	  (setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) selection))))))
+
+(defun guess-TeX-viewer (client-string file-path)
   (cond
     ((not (stringp client-string))
      "Okular")
-    ((string-prefix-p "192.168.56.1" client-string)
-     "display1_Okular")
+    ((and (string-prefix-p "192.168.0." client-string)
+	  (string-prefix-p "/machines/frank-lpc/paper-gits/" file-path))
+     "remote_SumatraPDF")
     ((string-prefix-p "129.215" client-string)
-     "remote_Okular"))
-)
+     "remote_Okular")
+    (t "Okular")))
+
+(defun TeX-view-update ()
+  "Sets viewer to the one used in other buffer or if no such buffer
+   uses guess-TeX-viewer"
+  (interactive)
+  (make-local-variable 'TeX-view-program-selection)
+
+  (let*
+      ((ssh-client-string (getenv "SSH_CONNECTION"))
+       (cur-file buffer-file-name)
+       (determined-viewer (guess-TeX-viewer ssh-client-string cur-file))
+       (existing-viewer nil))
+
+    ; Check if there is another latex buffer already
+    (dolist (b (buffer-list))
+      (unless (eq (current-buffer) b)
+	(with-current-buffer b
+	  (when (string= "latex-mode" major-mode)
+	    (setq existing-viewer (car (cdr (assoc 'output-pdf TeX-view-program-selection))))))))
+
+    (let ((viewer  (or existing-viewer determined-viewer)))
+      (setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) viewer)
+      (when (called-interactively-p 'any)
+	(warn-echo-area (concat "Setting pdf viewer to " viewer))))))
+
+
+
+
 
 (use-package tex
   :defer t
@@ -29,7 +87,7 @@
   :config
     ;(setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) "Atril-no-dbus")
     ;(setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) "remote_Okular")
-    (setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) (viewer-from-ssh-client (getenv "SSH_CONNECTION")))
+    ;(setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) (viewer-from-ssh-client (getenv "SSH_CONNECTION")))
     (add-to-list
      'TeX-view-program-list
      '("Atril-no-dbus" ("atril" (mode-io-correlate " -i %(outpage)") " %o") "atril"))
@@ -42,6 +100,9 @@
     (add-to-list
      'TeX-view-program-list
      '("remote_Okular" ("/home/frank/remote_okular.sh --unique %o" (mode-io-correlate "#src:%n%a")) "/home/frank/remote_okular.sh"))
+    (add-to-list
+     'TeX-view-program-list
+     '("remote_SumatraPDF" ("/home/frank/remote_SumatraPDF.sh -reuse-instance %o" (mode-io-correlate " -forward-search \"%b\" %n") ) "/home/frank/remote_SumatraPDF.sh"))
     (add-to-list
      'TeX-view-program-list
      '("display1_Okular" ("/home/frank/display1_okular.sh --noraise --unique %o" (mode-io-correlate "#src:%n%a")) "/home/frank/display1_okular.sh"))
@@ -60,7 +121,9 @@
     (add-hook 'LaTeX-mode-hook 'flyspell-mode)
     (company-auctex-init)
     (add-hook 'LaTeX-mode-hook 'company-mode)
+    (add-hook 'LaTeX-mode-hook 'TeX-view-update)
     (advice-add 'TeX-LaTeX-sentinel :after #'my/TeX-LaTeX-sentinel)
+    (advice-add 'TeX-command-run-all :before #'save-server-name)
 )
 
 
