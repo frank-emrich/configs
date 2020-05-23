d3e2785db214fd4d239c87d43a3fc87ee532b376
Frank Emrich <git@emrich.io>
2020-05-23 16:01:46 +0100

    which-key

--- .emacs.d/myinit.el
+++ .emacs.d/myinit.el
@@ -1403,6 +1403,19 @@ point reaches the beginning or end of the buffer, stop there."
 
 
 
+; After typing prefix key show help popup after some idle time
+(use-package which-key)
+;; Allow C-h to trigger which-key before it is done automatically
+;; (setq which-key-show-early-on-C-h t)
+;; make sure which-key doesn't show normally but refreshes quickly after it is
+;; triggered.
+;(setq which-key-idle-delay 10000)
+;(setq which-key-idle-secondary-delay 0.05)
+(which-key-mode)
+;(which-key-setup-minibuffer)
+(which-key-setup-side-window-bottom)
+
+
 
 
 ;; Make gc pauses faster by decreasing the threshold.
