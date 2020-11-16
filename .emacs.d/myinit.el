15c55dc70280efc613a9f15500a9b17fe51730c7
Frank Emrich <git@emrich.io>
2020-11-16 09:15:22 +0000

    undo tree in region

--- .emacs.d/myinit.el
+++ .emacs.d/myinit.el
@@ -657,6 +657,7 @@ With prefix ARG, silently save all file-visiting buffers, then kill."
     (global-undo-tree-mode)
     (setq undo-tree-visualizer-timestamps t)
     (setq undo-tree-visualizer-diff t) ;; show diff view by default
+    (setq undo-tree-enable-undo-in-region t)
     (define-key undo-tree-map "\C-_" nil)
     (define-key undo-tree-map (kbd "\C-x u") nil)
 )
