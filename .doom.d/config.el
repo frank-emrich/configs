172bfa72e5af4e64089320c8e76286066888f300
Frank Emrich <git@emrich.io>
2022-05-10 12:24:24 +0100

    Fix mismatched )

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -564,7 +564,7 @@ to run the replacement."
 ;;            doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
 ;;            doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
 ;;           doom-big-font (font-spec :family "Fira Mono" :size 19)
-      ))
+      )
   ;; osc52 workaround:
   (setq interprogram-cut-function 'osc52-select-text-tmux))
 
