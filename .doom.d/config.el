b02493583ab7604ce36a357b6fb842d54f1d02dd
Frank Emrich <git@emrich.io>
2022-11-21 19:08:19 +0000

    org mode setup using use-package!

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,7 +32,10 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(use-package! org
+  :init
+  (setq org-directory "~/org/")
+  (setq org-startup-folded nil))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
